513c03318ef91693e442d00a188a8fd26c032f44
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2020-02-28 04:43:41 -0700

    Bullets Work
    They move forward

---
+++ Ultimate Asteroid Tactical Control/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    private GameObject boolet;
+    private Rigidbody2D rb;
+    public float bulletSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.GetComponent<Rigidbody2D>().velocity = transform.up * bulletSpeed;
+    }
+}
---
+++ Ultimate Asteroid Tactical Control/Assets/Scripts/BulletSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawn : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}

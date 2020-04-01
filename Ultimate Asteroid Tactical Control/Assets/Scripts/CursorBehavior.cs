788b849b3fdbe6aef2d17e90143aef3f6b0cc94a
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2020-04-01 06:38:37 -0700

    Added Main Menu Art and Got Buttons
    Buttons aren't working yet

---
+++ Ultimate Asteroid Tactical Control/Assets/Scripts/CursorBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorBehavior : MonoBehaviour
+{
+    public GameObject cursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer sr = cursor.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TurnOnCursor()
+    {
+        cursor.GetComponent<SpriteRenderer>().enabled = true;
+        Debug.Log("The cursor has been turned off");
+    }
+}

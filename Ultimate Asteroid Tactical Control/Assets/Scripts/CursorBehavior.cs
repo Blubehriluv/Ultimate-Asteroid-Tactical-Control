de14f0e21d34e1ef2ee5eff832b712e4bc7f9cc3
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2020-04-02 02:19:55 -0700

    CURSOR FUCKING WORKS
    WHY AM I ALWAYS DOING EXTRA STUFF I NEED TO STOP.  A lovely animated dragon cursor follows your menu options.

--- Ultimate Asteroid Tactical Control/Assets/Scripts/CursorBehavior.cs
+++ Ultimate Asteroid Tactical Control/Assets/Scripts/CursorBehavior.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CursorBehavior : MonoBehaviour
 {
     public GameObject cursor;
+    //public EventSystem eventSys;
+    
     // Start is called before the first frame update
     void Start()
     {
         SpriteRenderer sr = cursor.GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void TurnOnCursor()
+    public void TurnOnCursor(BaseEventData eventData)
     {
         cursor.GetComponent<SpriteRenderer>().enabled = true;
+        Debug.Log("The cursor has been turned on");
+    }
+
+    public void TurnOffCursor(BaseEventData eventData)
+    {
+        cursor.GetComponent<SpriteRenderer>().enabled = false;
         Debug.Log("The cursor has been turned off");
     }
 }
